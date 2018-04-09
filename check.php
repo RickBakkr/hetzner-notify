d8493f04608e279d39b0c4ea81bf902267232c6c
Rick Bakker <rickbakkr@gmail.com>
2018-04-09 23:40:11 +0200

    Removed some unnecessary function

--- check.php
+++ check.php
@@ -2,21 +2,6 @@
 require 'vendor/autoload.php';
 require 'config.php';
 
-function array_splice_assoc(&$input, $offset, $length, $replacement = array()) {
-    $replacement = (array) $replacement;
-    $key_indices = array_flip(array_keys($input));
-    if (isset($input[$offset]) && is_string($offset)) {
-            $offset = $key_indices[$offset];
-    }
-    if (isset($input[$length]) && is_string($length)) {
-            $length = $key_indices[$length] - $offset;
-    }
-
-    $input = array_slice($input, 0, $offset, TRUE)
-            + $replacement
-            + array_slice($input, $offset + $length, NULL, TRUE);
-}
-
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
